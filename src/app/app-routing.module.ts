6709bb92f673d935e52f301ba384c410a5f3d049
sergey.polous <sergey.polous@tangramltd.com>
2018-09-18 15:03:22 +0300

    add server

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {StoreComponent} from './store/store.component';
+import {ServiceComponent} from './service/service.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', redirectTo: '/store', pathMatch: 'full'},
+    {path: 'store', component: StoreComponent},
+    {path: 'service', component: ServiceComponent},
+    {path: '**', redirectTo: '/store'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
